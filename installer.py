c4d83e1b4654f8e1d6f5b2ec10f322848a4cb365
TitanWolfGamer <titanwolfgamer998@gmail.com>
2026-04-30 22:33:52 +0200

    second initial commit

--- installer.py
+++ installer.py
@@ -1,6 +1,9 @@
 import os
-
+from time import time
 from tkinter.filedialog import askdirectory
+import json
+
+TEMP_FILENAME: str = f'AutoHolder-{time()}'
 
 def get_installation_directory() -> str:
     return askdirectory(title='Select Folder for installation')
@@ -9,5 +12,19 @@ def set_up_config(installation_directory: str) -> dict:
     return {'path': installation_directory}
 
 
-def install_program(installation_directory: str) -> None:
-    os.system('git clone ')
+def install_program(installation_directory: str, temp_filename: str) -> None:
+    temp_dir: str = os.environ['TEMP']
+    temp_path: str = rf'{temp_dir}\{temp_filename}'
+
+    config_content: dict = set_up_config(installation_directory)
+
+    os.system(f'git clone https://github.com/TitanWolfGamer/auto-holder.git {temp_path}')
+    os.remove(fr'{temp_path}\installer.py')
+    os.remove(fr'{temp_path}\.gitignore')
+
+    # overwriting default config data
+    with open(fr'{temp_path}\config.json', 'w') as f:
+        json.dump(config_content, f)
+
+
+install_program(get_installation_directory(), TEMP_FILENAME)
--- updater.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-r = requests.get('https://api.github.com/repos/TitanWolfGamer/nimrod/contents/nimrod.py')
-
-r2 = requests.get(r.json()['download_url'])
-
-print(r2.content.decode())
