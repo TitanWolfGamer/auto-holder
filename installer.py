c412964ce1b0ff5851fcf57372385d61c255c4da
TitanWolfGamer <titanwolfgamer998@gmail.com>
2026-05-01 17:03:30 +0200

    Minor Update

--- installer.py
+++ installer.py
@@ -1,12 +1,10 @@
 import os
 from time import time
 from tkinter.filedialog import askdirectory
-import json
 
 class Program:
     def __init__(self) -> None:
         self.__inst_dir: str = askdirectory(title='Select Folder for installation')
-        self.__config: dict = {'path': fr'{self.__inst_dir}/AutoHolder'}
 
     def install(self, temp_filename: str = f'AutoHolder-{time()}') -> None:
         temp_dir: str = os.environ['TEMP']
@@ -15,10 +13,6 @@ class Program:
         os.system(f'git clone https://github.com/TitanWolfGamer/auto-holder.git {temp_path}')
         os.remove(fr'{temp_path}\installer.py')
         os.remove(fr'{temp_path}\.gitignore')
-
-        # setting up config
-        with open(fr'{temp_path}\config.json', 'w') as f:
-            json.dump(self.__config, f)
 
         os.rename(temp_path, fr'{self.__inst_dir}\AutoHolder')
 
